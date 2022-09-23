094aea1a5020afc902d86405e507919a65637e9e
adityaaaaaans <adityafirdaus216@gmail.com>
2022-09-23 11:06:38 +0700

    push1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan Mandiri</title>
+</head>
+<body>
+    <div clas ="header">
+        <h1> Muchamad Firdaus Adityatama Assegaf</h1>
+        <i>Latihan include & require</i>
+        <hr/>
+    </div>
+
+    <div clas="content">
+        <br/>
+        home / About US
+        <h1>Tentang Saya</h1>
+        <p>Nama saya Firdaus,Dari SMKN5 Solo.</p>
+    </div>
+
+    <div clas="footer">
+        <hr/>
+        Include & require
+    </div>
+
+</body>
+</html>
---
+++ pertemuan1/header_include.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan Mandiri</Title>
+</head>
+<body>
+    <div class="header">
+        <h1>Muchamad Firdaus Adityatama Assegaf</h1>
+        <i>Latihan include & require</i>
+        <hr/>
+</div>
